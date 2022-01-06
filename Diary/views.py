a7a09123a09eeefadb2644d3ff583066db872789
freenozero <freenozero@naver.com>
2022-01-06 16:13:46 +0900

    login

--- Diary/views.py
+++ Diary/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
 
 def index(request):
+    if request.moethd == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username = username, password = password)
+        notUser = "don't login" #로그인 실패시 문자
+        if user is not None:
+            #로그인 성공
+            login(request, username)
+            return render(request, 'user.html', {'username':username, 'password':password})
+        else:
+            #로그인 실패시 notUser 문자 반환
+            return render(request, {'notUser': notUser})
     return render(request, 'index.html')
 
 
 def login(request):
     return render(request, 'login.html')
+
+
+def user_view(request):
+    return render(request, 'user.html')
