f0fc1c7be09df7e1f7ee2c88f87bc1f204fc3562
whdms2008 <whdms1107@naver.com>
2022-01-07 20:09:34 +0900

    음.. 대충 됨

--- Diary/urls.py
+++ Diary/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.signup, name='signup'),
     path('signup', views.signup, name='signup'),
     path('user', views.user, name='user'),
 ]
