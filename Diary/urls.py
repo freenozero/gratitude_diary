a09420f9f684fe93d97e80a4fe91bbb2ec0f02bc
freenozero <freenozero@naver.com>
2022-01-12 13:37:49 +0900

    loginview

--- Diary/urls.py
+++ Diary/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('user', views.user, name='user'),
     path('login', views.login_view, name='login_view'),
     path('logout', views.logout_view, name='logout_view'),
-    #path(post/', include('post.urls', namespace='post'))
 ]
