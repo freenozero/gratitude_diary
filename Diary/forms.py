a6f0a81882f4eb8ce0a81051d5cb93c42f18568c
whdms2008 <whdms1107@naver.com>
2022-01-13 15:08:25 +0900

    forms 올림

--- Diary/forms.py
+++ Diary/forms.py
@@ -22,7 +22,7 @@ class UserCreationForm(forms.ModelForm):
     def save(self, commit=True):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data["password1"])
-        user.age = date.today().year
+        user.age = date.today().year - int(user.date_of_birth.year) +1
         if commit:
             user.save()
         return user
