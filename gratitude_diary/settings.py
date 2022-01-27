ae1c69c3db5415f3e5920ac1e6297a2a353055ab
whdms2008 <whdms1107@naver.com>
2022-01-27 12:32:48 +0900

    email 테스트 성공

--- gratitude_diary/settings.py
+++ gratitude_diary/settings.py
@@ -135,3 +135,16 @@ MESSAGE_TAGS = {
     messages.WARNING: 'alert-warning',
     messages.ERROR: 'alert-danger',
 }
+
+EMAIL_HOST = 'smtp.gmail.com'
+# 메일을 호스트하는 서버
+EMAIL_PORT = '587'
+# gmail과의 통신하는 포트
+EMAIL_HOST_USER = 'whdms1107@gmail.com'
+# 발신할 이메일
+EMAIL_HOST_PASSWORD = 'dudnqlbn6600qe'
+# 발신할 메일의 비밀번호
+EMAIL_USE_TLS = True
+# TLS 보안 방법
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# 사이트와 관련한 자동응답을 받을 이메일 주소,'webmaster@localhost'
