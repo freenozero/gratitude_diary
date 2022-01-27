551d106fde26bacdff902be9ce70b5d05d7c2b32
freenozero <freenozero@naver.com>
2022-01-27 12:51:26 +0900

    html 곳곳에서 유저 인증되어 있는지 확인2

--- Diary/urls.py
+++ Diary/urls.py
@@ -4,7 +4,6 @@ from . import views
 #app_name = 'Diary'
 urlpatterns = [
     path('', views.login_view, name='index'),
-    path('user/', views.user, name='user'),
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout_view, name='logout'),
     path('signout/', views.signOut, name='signOut'),
