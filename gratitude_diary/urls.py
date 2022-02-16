2683b1cd65f700561f744f0dfbb4dbb6410bc58f
freenozero <freenozero@naver.com>
2022-02-16 16:31:55 +0900

    deco 디렉터리 추가

--- gratitude_diary/urls.py
+++ gratitude_diary/urls.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('Diary.urls')),
     path('Diary/', include('Diary_write.urls')),
+    path('Diary_deco/', include('Diary_deco.urls')),
     path('admin/', admin.site.urls),
     # path('accounts/', include('allauth.urls')),
 
